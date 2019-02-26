ac4bbd689f27b6a4aaf6ac44529ad6e9645d6333
Gaby Vazquez <gabs.vz@gmail.com>
2019-02-25 18:45:18 -0800

    Jest tests for snapshots.

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,26 +1,17 @@
-import React                       from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
-import App                         from './App';
-import SearchBar                   from './SearchBar';
+import React from "react";
+import { shallow, ShallowWrapper } from "enzyme";
+import App from "./App";
 
-describe('App', () => {
+describe("App", () => {
   let app: ShallowWrapper;
 
   beforeEach(() => {
     app = shallow(<App />);
   });
 
-  it('should have a header', () => {
-    expect(app.containsMatchingElement(
-      <header>
-        <SearchBar />
-      </header>
-    )).toBe(true);
-  });
+  it('renders without crashing', () => {});
 
-  it('should have a main', () => {
-    expect(app.containsMatchingElement(
-      <main />
-    )).toBe(true);
+  it("renders correctly", () => {
+    expect(app).toMatchSnapshot();
   });
 });
