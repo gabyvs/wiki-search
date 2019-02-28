9adb8716f438ca8a121a4ca9ebe8a7063e2d2cb1
Gaby Vazquez <gabs.vz@gmail.com>
2019-02-27 22:29:54 -0800

    Introducing theme variables.

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -5,11 +5,13 @@ import Result      from '../Result/Result';
 export interface ListProps {
   results: IResult[]
 }
-const List = (props: ListProps) =>
+const List = (props: ListProps) => (
   <>
     {
       props.results.map((result, i) => <Result {...result} key={i}/>)
     }
   </>
+);
+
 
 export default List;
